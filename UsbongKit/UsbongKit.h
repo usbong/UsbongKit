d6f9ccbc3f2e707840ffc4f6dfecc2c3dff87a40
Chris Amanse <christopheramanse@gmail.com>
2015-10-24 22:47:26 +0800

    Initial Commit

---
+++ UsbongKit/UsbongKit.h
@@ -0,0 +1,19 @@
+//
+//  UsbongKit.h
+//  UsbongKit
+//
+//  Created by Chris Amanse on 10/24/15.
+//  Copyright © 2015 Usbong Social Systems, Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for UsbongKit.
+FOUNDATION_EXPORT double UsbongKitVersionNumber;
+
+//! Project version string for UsbongKit.
+FOUNDATION_EXPORT const unsigned char UsbongKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <UsbongKit/PublicHeader.h>
+
+
