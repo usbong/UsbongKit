4838ad45e2c9af51ed58fa748148af8be2229dad
Chris Amanse <christopheramanse@gmail.com>
2015-10-30 22:36:33 +0800

    Revise architecture to prepare for delegation pattern

--- UsbongKit/Classes/UsbongTreeViewer.swift
+++ UsbongKit/Classes/UsbongTreeViewer.swift
@@ -8,14 +8,37 @@
 
 import Foundation
 
+//public class UsbongTreeViewer {
+//    public static func presentTreeViewControllerFromViewController(viewController: UIViewController, withZipURL zipURL: NSURL, animated: Bool, completion: (() -> Void)?) -> TreeViewController {
+//        let initialVC = TreeViewController.loadFromStoryboard()
+//        let treeVC = initialVC.topViewController as! TreeViewController
+//        
+//        treeVC.treeZipURL = zipURL
+//        
+//        viewController.presentViewController(initialVC, animated: animated, completion: completion)
+//        
+//        return treeVC
+//    }
+//}
+
+public protocol UsbongTreeViewerDelegate: class {
+}
 public class UsbongTreeViewer {
-    public static func presentTreeViewControllerFromViewController(viewController: UIViewController, withZipURL zipURL: NSURL, animated: Bool, completion: (() -> Void)?) -> TreeViewController {
+    public weak var delegate: UsbongTreeViewerDelegate?
+    
+    public var treeZipURL: NSURL
+    
+    public init(treeZipURL: NSURL) {
+        self.treeZipURL = treeZipURL
+    }
+    
+    public func presentTreeViewControllerFromViewController(viewController: UIViewController, animated: Bool, completion: (() -> Void)?) -> TreeViewController {
         let initialVC = TreeViewController.loadFromStoryboard()
         let treeVC = initialVC.topViewController as! TreeViewController
         
-        treeVC.treeZipURL = zipURL
+        treeVC.treeZipURL = treeZipURL
         
-        viewController.presentViewController(initialVC, animated: animated, completion: completion)
+        viewController.presentViewController(initialVC, animated: true, completion: nil)
         
         return treeVC
     }
