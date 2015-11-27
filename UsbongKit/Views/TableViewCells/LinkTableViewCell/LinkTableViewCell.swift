cb4ef6ec53402a4dfd5970aecb01e188a5460f05
Chris Amanse <christopheramanse@gmail.com>
2015-11-27 10:06:12 +0800

    Add link table view cell blank source file and nib

---
+++ UsbongKit/Views/TableViewCells/LinkTableViewCell/LinkTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  LinkTableViewCell.swift
+//  UsbongKit
+//
+//  Created by Joe Amanse on 27/11/2015.
+//  Copyright © 2015 Usbong Social Systems, Inc. All rights reserved.
+//
+
+import UIKit
+
+class LinkTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
