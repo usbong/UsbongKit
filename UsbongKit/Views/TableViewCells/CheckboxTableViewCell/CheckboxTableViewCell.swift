3316f8379dc99410c3154bcd67f10875778c5c26
Chris Amanse <christopheramanse@gmail.com>
2015-12-15 16:49:23 +0800

    Integrate CheckboxButton
    - add CheckboxTableViewCell blank source and nib

---
+++ UsbongKit/Views/TableViewCells/CheckboxTableViewCell/CheckboxTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  CheckboxTableViewCell.swift
+//  UsbongKit
+//
+//  Created by Chris Amanse on 12/15/15.
+//  Copyright © 2015 Usbong Social Systems, Inc. All rights reserved.
+//
+
+import UIKit
+
+class CheckboxTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
