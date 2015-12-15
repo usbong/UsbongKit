d4fca5438f4a9ecd2050d5fca4d77a755f407100
Chris Amanse <christopheramanse@gmail.com>
2015-12-15 17:06:16 +0800

    Add checkbox button and label

--- UsbongKit/Views/TableViewCells/CheckboxTableViewCell/CheckboxTableViewCell.swift
+++ UsbongKit/Views/TableViewCells/CheckboxTableViewCell/CheckboxTableViewCell.swift
@@ -7,15 +7,27 @@
 //
 
 import UIKit
+import CheckboxButton
 
-class CheckboxTableViewCell: UITableViewCell {
-
-    override func awakeFromNib() {
+public class CheckboxTableViewCell: UITableViewCell {
+    @IBOutlet public weak var checkboxButton: CheckboxButton!
+    @IBOutlet public weak var titleLabel: UILabel!
+    
+    public var checkboxButtonSelected: Bool {
+        get {
+            return checkboxButton.selected
+        }
+        set {
+            checkboxButton.selected = newValue
+        }
+    }
+    
+    override public func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
     }
 
-    override func setSelected(selected: Bool, animated: Bool) {
+    override public func setSelected(selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
 
         // Configure the view for the selected state
