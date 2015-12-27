ee42e9815e6a66533f026415f8a2eb753130392b
Chris Amanse <christopheramanse@gmail.com>
2015-12-27 17:46:53 +0800

    Update NodeProvider and add sample NodeProvider

--- UsbongKit/NodeProvider.swift
+++ UsbongKit/NodeProvider.swift
@@ -9,12 +9,12 @@
 import Foundation
 
 public protocol NodeProvider {
-    var currentNode: Node { get }
+    var currentNode: Node? { get }
     var nextNodeIsAvailable: Bool { get }
     var previousNodeIsAvailable: Bool { get }
     
-    var nothingSelected: Bool { get }
-    var currentNodeIsSelectionType: Bool { get }
+    func transitionToNextNode() -> Bool
+    func transitionToPreviousNode() -> Bool
 }
 
 public extension NodeProvider {
@@ -28,5 +28,51 @@ public extension NodeProvider {
     
     public var currentNodeIsSelectionType: Bool {
         return currentNode is SelectionTypeNode
+    }
+}
+
+public class NodeCollection {
+    private var currentIndex: Int = 0
+    public var nodes: [Node] = []
+    
+    public init(nodes: [Node]) {
+        self.nodes = nodes
+    }
+}
+
+extension NodeCollection: NodeProvider {
+    public var currentNode: Node? {
+        if nodes.count > 0 {
+            return nodes[currentIndex]
+        }
+        
+        return nil
+    }
+    
+    public var nextNodeIsAvailable: Bool {
+        let maxIndex = nodes.count - 1
+        return currentIndex < maxIndex
+    }
+    
+    public var previousNodeIsAvailable: Bool {
+        return currentIndex > 0
+    }
+    
+    public func transitionToNextNode() -> Bool {
+        if nextNodeIsAvailable {
+            currentIndex += 1
+            return true
+        }
+        
+        return false
+    }
+    
+    public func transitionToPreviousNode() -> Bool {
+        if previousNodeIsAvailable {
+            currentIndex -= 1
+            return true
+        }
+        
+        return false
     }
 }
