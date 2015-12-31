5215353c6341144a133e071d9334953f12b6ff90
Chris Amanse <christopheramanse@gmail.com>
2015-12-31 14:23:05 +0800

    Add choose language screen

--- UsbongKit-example/TreeViewController.swift
+++ UsbongKit-example/TreeViewController.swift
@@ -11,11 +11,26 @@ import UIKit
 
 class TreeViewController: UIViewController {
     
-    var treeURL: NSURL?
-    var treeRootURL: NSURL?
-    
     @IBOutlet weak var nodeView: NodeView!
     @IBOutlet weak var previousNextSegmentedControl: UISegmentedControl!
+    
+    var treeURL: NSURL?
+    var treeRootURL: NSURL?
+    var tree: UsbongTree?
+    
+    var voiceOverOn: Bool {
+        get {
+            // Default to true if not yet set
+            let standardUserDefaults = NSUserDefaults.standardUserDefaults()
+            if standardUserDefaults.objectForKey("SpeechOn") == nil {
+                standardUserDefaults.setBool(true, forKey: "SpeechOn")
+            }
+            return standardUserDefaults.boolForKey("SpeechOn")
+        }
+        set {
+            NSUserDefaults.standardUserDefaults().setBool(newValue, forKey: "SpeechOn")
+        }
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -26,7 +41,8 @@ class TreeViewController: UIViewController {
                 print(treeRootURL)
                 
                 let tree = UsbongTree(treeRootURL: treeRootURL)
-                print(tree.taskNodeNames)
+                self.tree = tree
+                
                 if let startingNode = tree.currentNode {
                     nodeView.node = startingNode
                 }
@@ -45,14 +61,60 @@ class TreeViewController: UIViewController {
         dismissViewControllerAnimated(true, completion: nil)
     }
     
-    /*
+    @IBAction func didPressMore(sender: AnyObject) {
+        let actionController = UIAlertController(title: nil, message: nil, preferredStyle: .ActionSheet)
+        
+        let onOrOffText = voiceOverOn ? "Off" : "On"
+        let speechAction = UIAlertAction(title: "Speech \(onOrOffText)", style: .Default) { (action) -> Void in
+            let turnOn = !self.voiceOverOn
+            
+            // If toggled to on, start voice-over
+            if turnOn {
+                print("Start voice-over")
+//                self.startVoiceOver()
+            } else {
+                print("Stop voice-over")
+//                self.stopVoiceOver()
+            }
+            
+            self.voiceOverOn = turnOn
+        }
+        let setLanguageAction = UIAlertAction(title: "Set Language", style: .Default) { (action) -> Void in
+            self.showChooseLanguageScreen()
+        }
+        let cancelAction = UIAlertAction(title: "Cancel", style: .Cancel, handler: nil)
+        
+        actionController.addAction(speechAction)
+        actionController.addAction(setLanguageAction)
+        actionController.addAction(cancelAction)
+        
+        // For iPad action sheet behavior (similar to a popover)
+        if let popover = actionController.popoverPresentationController, let barButtonItem = sender as? UIBarButtonItem {
+            popover.barButtonItem = barButtonItem
+        }
+        
+        presentViewController(actionController, animated: true, completion: nil)
+    }
+    
+    // MARK: Functions
+    
+    func showChooseLanguageScreen() {
+        performSegueWithIdentifier("presentLanguages", sender: self)
+    }
+    
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+        guard let identifier = segue.identifier else {
+            return
+        }
+        
+        switch identifier {
+        case "presentLanguages":
+            print(tree?.availableLanguages ?? [])
+        default:
+            break
+        }
     }
-    */
-
 }
