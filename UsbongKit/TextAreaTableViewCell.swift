a527ef6fb99ce6f5d9d23b6b6d598c65edf284ad
Chris Amanse <christopheramanse@gmail.com>
2016-02-12 16:37:09 +0800

    Add TextAreaTableViewCell

---
+++ UsbongKit/TextAreaTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  TextAreaTableViewCell.swift
+//  UsbongKit
+//
+//  Created by Joe Amanse on 12/02/2016.
+//  Copyright © 2016 Usbong Social Systems, Inc. All rights reserved.
+//
+
+import UIKit
+
+public class TextAreaTableViewCell: UITableViewCell, NibReusable {
+
+    override public func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override public func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
