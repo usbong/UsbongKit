ac7e7823d2cdaabe25152a65d45df415063ea0e2
Chris Amanse <christopheramanse@gmail.com>
2016-03-13 09:03:53 +0800

    Add DateTableViewCell

---
+++ UsbongKit/DateTableViewCell.swift
@@ -0,0 +1,26 @@
+//
+//  DateTableViewCell.swift
+//  UsbongKit
+//
+//  Created by Joe Amanse on 13/03/2016.
+//  Copyright © 2016 Usbong Social Systems, Inc. All rights reserved.
+//
+
+import UIKit
+
+class DateTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var datePicker: UIDatePicker!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
