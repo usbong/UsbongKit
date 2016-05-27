1648720c7073c3b92b84ac4426d6725225d38ca8
Chris Amanse <chris@chrisamanse.xyz>
2016-05-27 16:35:36 +0800

    Add documentation in Usbong.swift

--- UsbongKit/Usbong.swift
+++ UsbongKit/Usbong.swift
@@ -8,9 +8,17 @@
 
 import UIKit
 
+/// Usbong is a collection of functions to manage utree files
 public struct Usbong {
     private init() {}
     
+    /**
+     Read utree file and present viewer
+     
+     - parameters:
+       - viewController: The parent view controller for the to be presented viewer
+       - url: URL for the utree file
+    */
     public static func presentViewer(onViewController viewController: UIViewController, withUtreeURL url: NSURL) {
         let storyboard = UIStoryboard(name: "UsbongKit", bundle: NSBundle(forClass: TreeViewController.self))
         
