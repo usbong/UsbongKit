e5e333076ce6b64085145b221ccca2cf18f31026
Chris Amanse <chris@chrisamanse.xyz>
2016-10-20 03:59:44 -0700

    Implement multiple correct text answers
    - fixes usbong/LIKHA#3

--- UsbongKit/Usbong/UsbongTree/UsbongTree.swift
+++ UsbongKit/Usbong/UsbongTree/UsbongTree.swift
@@ -104,7 +104,7 @@ public class UsbongTree {
                 switch taskNodeType {
                 case .TextFieldWithAnswer, .TextAreaWithAnswer:
                     // Return Yes or No if with answer
-                    if textInputTypeNode.textInput == currentTargetTextInput {
+                    if let answers = currentTargetTextInput where answers.componentsSeparatedByString("|").contains(textInputTypeNode.textInput) {
                         return "Yes"
                     } else {
                         return "No"
